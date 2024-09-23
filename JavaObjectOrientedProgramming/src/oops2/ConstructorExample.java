4abf2e112a5ce6a3624edf1c29c8e5cdc15544bb
Faruq <onaayofaruq24@gmail.com>
2024-09-22 22:27:09 -0600

    Third Commit: Complete working with constructors

---
+++ JavaObjectOrientedProgramming/src/oops2/ConstructorExample.java
@@ -0,0 +1,47 @@
+package oops2;
+
+import java.util.Scanner;
+
+class Student {
+	// Data Members
+	private int studentId;
+	private String studentName;
+	private int studentAge;
+
+	Scanner scanner = new Scanner(System.in);
+
+	// Default constructor
+	public Student() {
+		this.studentId = 6636;
+		this.studentName = "Faruq";
+		this.studentAge = 21;
+	}
+
+	// Parameterized constructor
+	public Student(int sId, String sName, int sAge) {
+		this.studentId = sId;
+		this.studentName = sName;
+		this.studentAge = sAge;
+	}
+
+	void displayDetails() {
+		System.out.println("Student ID: " + studentId);
+		System.out.println("Student Name: " + studentName);
+		System.out.println("Student Age: " + studentAge);
+	}
+
+}
+
+public class ConstructorExample {
+	public static void main(String[] args) {
+
+		Student studentObj1 = new Student();
+
+		studentObj1.displayDetails();
+
+		Student studentObj2 = new Student(434, "Musab", 17);
+
+		studentObj2.displayDetails();
+
+	}
+}
